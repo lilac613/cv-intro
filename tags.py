beec867ac60b42fe85552656f7da36c0caccd612
lilac613 <yablondani@gmail.com>
2023-07-31 16:16:04 -0400

    start

---
+++ tags.py
@@ -0,0 +1,19 @@
+from dt_apriltags import Detector
+import matplotlib.pyplot as plt
+import numpy as np
+
+cameraMatrix = np.array([ 1060.71, 0, 960, 0, 1060.71, 540, 0, 0, 1]).reshape((3,3))
+tag_size = 0.1
+camera_params = ( cameraMatrix[0,0], cameraMatrix[1,1], cameraMatrix[0,2], cameraMatrix[1,2] )
+
+def get_tags(img):
+    at_detector = Detector(families='tag36h11',
+                       nthreads=1,
+                       quad_decimate=1.0,
+                       quad_sigma=0.0,
+                       refine_edges=1,
+                       decode_sharpening=0.25,
+                       debug=0)
+
+    tags = at_detector.detect(img, True, camera_params, tag_size)
+    return tags
